1193064e26157f5e327832d01e60f38da0faa75a
orbanpetra <o.petraaaa@gmail.com>
2026-04-01 11:17:28 +0200

    Create Query_1.sql

---
+++ SQL/Query_1.sql
@@ -0,0 +1,8 @@
+SELECT
+    corr(happiness_score, gdp_per_capita)                  AS gdp_corr,
+    corr(happiness_score, social_support)                  AS social_support_corr,
+    corr(happiness_score, healthy_life_expectancy)         AS life_expectancy_corr,
+    corr(happiness_score, freedom_to_make_life_choices)    AS freedom_corr,
+    corr(happiness_score, generosity)                      AS generosity_corr,
+    corr(happiness_score, perceptions_of_corruption)       AS corruption_corr
+FROM happiness_report;
